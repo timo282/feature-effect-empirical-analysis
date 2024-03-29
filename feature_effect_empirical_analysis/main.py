0fdf86ce95e6ca99b39234a3e23fa5edbbab6eb7
Timo Heiß <ti-heiss@t-online.de>
2024-03-29 22:50:12 +0100

    Fix main loops

--- feature_effect_empirical_analysis/main.py
+++ feature_effect_empirical_analysis/main.py
@@ -20,8 +20,8 @@ sim_config.read("config.ini")
 def simulate(
     models: List[BaseEstimator],
     n_sim: int,
-    n_train: int,
-    noise_sd: float,
+    n_trains: List[int],
+    noise_sds: List[float],
     config: ConfigParser,
 ):
     df_model_results = pd.DataFrame(
@@ -37,73 +37,91 @@ def simulate(
     )
 
     for i in range(n_sim):
-        # generate data
-        X_train, y_train, X_test, y_test = generate_data(
-            n_train=n_train,
-            n_test=config.getint("simulation_metadata", "n_test"),
-            noise_sd=noise_sd,
-            seed=i,
-        )
+        for n_train in n_trains:
+            for noise_sd in noise_sds:
+                # generate data
+                X_train, y_train, X_test, y_test = generate_data(
+                    n_train=n_train,
+                    n_test=config.getint("simulation_metadata", "n_test"),
+                    noise_sd=noise_sd,
+                    seed=i,
+                )
 
-        for model in models:
-            # train and tune model
-            model = train_model(
-                model,
-                X_train,
-                y_train,
-                n_trials=config.getint("simulation_metadata", "n_trials"),
-                cv=config.getint("simulation_metadata", "n_tuning_folds"),
-                metric=config.get("simulation_metadata", "tuning_metric"),
-                direction=config.get(
-                    "simulation_metadata", "tuning_direction"
-                ),
-            )
+                for model in models:
+                    # train and tune model
+                    model = train_model(
+                        model,
+                        X_train,
+                        y_train,
+                        n_trials=config.getint(
+                            "simulation_metadata", "n_trials"
+                        ),
+                        cv=config.getint(
+                            "simulation_metadata", "n_tuning_folds"
+                        ),
+                        metric=config.get(
+                            "simulation_metadata", "tuning_metric"
+                        ),
+                        direction=config.get(
+                            "simulation_metadata", "tuning_direction"
+                        ),
+                    )
 
-            # save model
-            model_folder = config.get("storage", "models")
-            model_name = model.__class__.__name__
-            dump(
-                model,
-                f"{model_folder}/{model_name}_{datetime.now()}.joblib",
-            )
+                    # save model
+                    model_folder = config.get("storage", "models")
+                    model_name = model.__class__.__name__
+                    dump(
+                        model,
+                        f"{model_folder}/{model_name}_{datetime.now()}.joblib",
+                    )
 
-            # evaluate model
-            model_results = eval_model(model, X_train, y_train, X_test, y_test)
-            df_model_results = df_model_results.append(
-                {
-                    "model": model_name,
-                    "simulation": i,
-                    "mse_train": model_results[0],
-                    "mse_test": model_results[1],
-                    "mae_train": model_results[2],
-                    "mae_test": model_results[3],
-                    "r2_train": model_results[4],
-                    "r2_test": model_results[5],
-                },
-                ignore_index=True,
-            )
+                    # evaluate model
+                    model_results = eval_model(
+                        model, X_train, y_train, X_test, y_test
+                    )
+                    df_model_results = df_model_results.append(
+                        {
+                            "model": model_name,
+                            "simulation": i,
+                            "mse_train": model_results[0],
+                            "mse_test": model_results[1],
+                            "mae_train": model_results[2],
+                            "mae_test": model_results[3],
+                            "r2_train": model_results[4],
+                            "r2_test": model_results[5],
+                        },
+                        ignore_index=True,
+                    )
 
-            # save model results
-            model_results_storage = config.get("storage", "model_results")
-            engine = create_engine(f"sqlite://{model_results_storage}")
-            df_model_results.to_sql(
-                "model_results", con=engine, if_exists="append"
-            )
+                    # save model results
+                    model_results_storage = config.get(
+                        "storage", "model_results"
+                    )
+                    engine = create_engine(f"sqlite://{model_results_storage}")
+                    df_model_results.to_sql(
+                        "model_results", con=engine, if_exists="append"
+                    )
 
 
 if __name__ == "__main__":
     model_names = sim_config.get("simulation_params", "models").split(",")
     n_sim_config = sim_config.getint("simulation_params", "n_sim")
-    n_train_config = sim_config.getint("simulation_params", "n_train")
-    noise_sd_config = sim_config.getfloat("simulation_params", "noise_sd")
+    n_train_config = [
+        int(x)
+        for x in sim_config.get("simulation_params", "n_train").split(",")
+    ]
+    noise_sd_config = [
+        float(x)
+        for x in sim_config.get("simulation_params", "noise_sd").split(",")
+    ]
     models_config = [
         map_modelname_to_estimator(model_name) for model_name in model_names
     ]
 
     simulate(
-        models_config,
-        n_sim_config,
-        n_train_config,
-        noise_sd_config,
-        models_config,
+        models=models_config,
+        n_sim=n_sim_config,
+        n_trains=n_train_config,
+        noise_sds=noise_sd_config,
+        config=sim_config,
     )
